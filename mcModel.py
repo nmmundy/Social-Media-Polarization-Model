c9f7a428e3ec33eff17cea255325110523e1d751
nmmundy <nmmundy@charter.net>
2025-07-23 18:14:02 -0700

    New file to begin work from

---
+++ mcModel.py
@@ -0,0 +1,7 @@
+import networkx as nx
+import numpy as np
+from scipy.sparse import identity
+from scipy.sparse.linalg import spsolve
+import matplotlib.pyplot as plt
+
+#New version of the model that actually follows the math.
