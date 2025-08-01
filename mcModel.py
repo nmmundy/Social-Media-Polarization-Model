4f5a493f12dda03bd4c133ee785cfc0e7ef7e473
nmmundy <nmmundy@charter.net>
2025-07-31 19:18:01 -0700

    added csv file printing

--- mcModel.py
+++ mcModel.py
@@ -2,16 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 from matplotlib.animation import FuncAnimation
+import csv
 
 class MarkovChainPolarizationModel:
-    def __init__(self, n, eta=2.0, beta=2.0, alpha=2.0, sigma=0.1, seed=None):
+    def __init__(self, n, eta=2.0, zPrime=1., gamma=.5, zeta=5, sigma=0.1, seed=None):
         self.n = n # number of nodes
         self.eta = eta # sensitivity to opinion updates parameter
         self.sigma = sigma # standard deviation for edge weights
+        self.gamma = gamma #
+        self.zPrime = zPrime # target opinion between 0,1
+        self.zeta = zeta #
         self.rng = np.random.default_rng(seed) # random number generator
-        self.beta = beta # parameter for the beta distribution
-        self.alpha = alpha # parameter for beta distribution
         self.z = self.rng.uniform(0, 1, size=n) # opinions of each node at time step t
+        self.z[0] = 0
+        self.z[1] = 1
 
         w = self.rng.uniform(0, 1, size=(n, n)) # weights of edges at current time
         np.fill_diagonal(w, 0)
@@ -20,7 +24,7 @@ class MarkovChainPolarizationModel:
     def opinionUpdate(self):
         z_prime = np.zeros_like(self.z)
 
-        for t in range(self.n):
+        for t in range(2, self.n):
             ## Below two lines represent Equation 4 in the overleaf
             weighted_opinions = np.sum(self.w[t] * self.z) 
             z_i = (self.z[t] + weighted_opinions) / (1 + np.sum(self.w[t]))
@@ -49,9 +53,9 @@ class MarkovChainPolarizationModel:
         self.w = w_prime
         '''
     ## Revised edge weights now company has influence
-    ## self, platform input, target opinion zPrime, strengh of company bias (0,1)
-    def edgeWeightUpdate(self, platformInfluence=True, zPrime=1, gamma=.5, zeta=10):
-        concentration = 5
+    ## self, platform input, target opinion zPrime, strengh of company bias (0,1) gamma
+    def edgeWeightUpdate(self, platformInfluence=True, zPrime=.5, gamma=1., zeta=5):
+        concentration = 10
         w_new = np.zeros_like(self.w)
         for i in range(self.n):
             for j in range(self.n):
@@ -69,6 +73,12 @@ class MarkovChainPolarizationModel:
                     beta_ = (1 - mu) * concentration
 
                     w_ij = self.rng.beta(alpha, beta_)
+                    print("Current Edge weight: ", w_ij)
+
+                    with open('weights.csv', 'a', newline='') as file:
+                        fieldname = ["edge weight"]
+                        writer = csv.DictWriter(file, fieldnames=fieldname)
+                        writer.writerow({"edge weight": w_ij})
                     w_new[i,j] = w_ij
 
                 if w_new[i].sum() > 0:
@@ -132,7 +142,7 @@ def visualization(opinions, weights, interval=500):
 
         curr_opinions = opinions[frame]
         curr_weights_matrix = weights[frame]
-
+        
         # Update the graph's edges with the current weights
         Graph.clear_edges()
         for i in range(n):
@@ -140,7 +150,7 @@ def visualization(opinions, weights, interval=500):
                 weight = curr_weights_matrix[i, j]
                 if weight > 0:  # Only add edges with a non-zero weight
                     Graph.add_edge(i, j, weight=weight)
-
+            #print("All weights of graph ", curr_weights_matrix)
         # KEY CHANGE: Calculate the layout using nx.spring_layout
         # The 'weight' attribute of the edges will be used to determine spring strength.
         # k: ideal distance between nodes.
@@ -154,7 +164,7 @@ def visualization(opinions, weights, interval=500):
             # pos[node][1] = initial_y_pos[node]
 
         node_colors = plt.cm.coolwarm(curr_opinions)
-
+        
         # Draw the network with the new layout
         edge_widths = [d['weight'] * 5 for u, v, d in Graph.edges(data=True)]
 
@@ -182,6 +192,6 @@ def visualization(opinions, weights, interval=500):
     return ani
 
 if __name__ == "__main__":
-    model = MarkovChainPolarizationModel(n=15, eta=8, sigma=10, seed=42)
-    opinions, weights = model.runModel(t=30)
+    model = MarkovChainPolarizationModel(n=15, eta=.3, sigma=.3, seed=42)
+    opinions, weights = model.runModel(t=15)
     visualization(opinions, weights)
